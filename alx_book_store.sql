38d26b4baee18386f160943252f26a8385bac41b
Wanjiru-mugo <wanjirumugomary@gmail.com>
2025-09-02 05:27:03 +0300

    DDL in practice & initializing database

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -18,6 +18,7 @@ my_cursor.execute("""
 my_cursor.execute("""
 	CREATE TABLE Books (
 		book_id INT PRIMARY KEY,
+		title VARCHAR(130) NOT NULL,
 		author_id INT,
 		price DOUBLE,
 		publication_date DATE,
