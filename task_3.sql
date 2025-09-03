d0c6e3574532e191fcb44457bcb681cd16ac56bd
Wanjiru-mugo <wanjirumugomary@gmail.com>
2025-09-03 16:42:12 +0300

    database querying DQL

---
+++ task_3.sql
@@ -0,0 +1,6 @@
+"""this script lists all the tables create into the alx_book_store database and run directly as mysql file"""
+SELECT *FROM Authors
+SELECT *FROM Books
+SELECT *FROM Orders
+SELECT *FROM Customers
+SELECT *FROM Order_Details
